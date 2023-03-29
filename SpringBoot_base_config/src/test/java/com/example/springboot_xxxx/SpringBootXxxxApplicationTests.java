ce320a4a7a910d6de0feca287817efd6c9362e00
liaozhongwei <1685268582@qq.com>
2023-03-29 16:28:33 +0800

    basic Springboot

---
+++ SpringBoot_base_config/src/test/java/com/example/springboot_xxxx/SpringBootXxxxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_xxxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootXxxxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
